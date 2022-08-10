83f5de885b0414c8f4e3e6a206fa4fea28481021
selman.karagol <burakkaragol60@gmail.com>
2022-08-11 02:28:43 +0300

    assigned task design

--- web/src/components/TaskCard.vue
+++ web/src/components/TaskCard.vue
@@ -1,8 +1,9 @@
 <template>
   <v-card
     :disabled="isDisabled"
+    :style="usersOwnTask"
   >
-    <v-card-title class="py-2 d-flex flex-row justify-content-between">
+    <v-card-title class="py-2 d-flex flex-row justify-content-between task-card-title">
       {{task.taskName}}
       <div style="font-size: small">
         <router-link style="text-decoration: none" :to="`/user/profile/${task.assignee.id}`"> {{task.assignee.username}}</router-link>
@@ -43,10 +44,27 @@ export default {
     handleProjectDetails() {
       this.$router.push("/task/details/" + this.$props.task.id)
     }
+  },
+  computed: {
+    // eslint-disable-next-line vue/return-in-computed-property
+    usersOwnTask() {
+      // eslint-disable-next-line no-debugger
+      debugger;
+      if( this.$store.state.userId === this.task.assignee.id){
+        if(this.task.status === "backlog") {
+          return "border: 2px solid #001a00"
+        }
+        for (let index in this.$store.state.selectedProject.taskStatus) {
+          if(this.$store.state.selectedProject.taskStatus[index].status === this.task.status) {
+            return "border: 2px solid " + this.$store.state.selectedProject.taskStatus[index].color.split(' ')[0];
+          }
+        }
+      }
+      else "";
+    }
   }
 }
 </script>
 
 <style scoped>
-
 </style>
